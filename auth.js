8356cfed541bd14f789037314d8630550020f30b
panitakittara-svg <panitakittara@gmail.com>
2026-07-15 19:23:17 +0700

    Delete auth.js

--- auth.js
+++
@@ -1,7 +0,0 @@
-// ==========================================
-// AUA Internship Hub
-// auth.js (Temporary Version)
-// Firebase Authentication will be added later.
-// ==========================================
-
-console.log("Auth.js loaded successfully.");
